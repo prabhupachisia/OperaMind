0169dbee59cf0bc15d76814cb3f73cb2a180f9a4
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2026-06-22 16:04:53 +0530

    merged conflicts on app

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+
+from routes.graph import graph_bp
 from routes.chat import chat_bp
 
 from extensions import Base
@@ -9,29 +11,35 @@ from extensions import engine
 from routes.graph import graph_bp
 from routes.upload import upload_bp
 
-# Blueprints
-from routes.graph import graph_bp
-from routes.upload import upload_bp
-
+# IMPORTANT
 import models
 
 app = Flask(__name__)
 CORS(app)
 
-# Create tables
+# Create all database tables
 Base.metadata.create_all(bind=engine)
 
 
 @app.route("/")
 def home():
     return {
-        "status": "running"
+        "status": "running",
+        "project": "OperaMind"
     }
 
+# Routes
 app.register_blueprint(
     graph_bp,
     url_prefix="/graph"
 )
+
+# Chat / RAG routes
+app.register_blueprint(
+    chat_bp,
+    url_prefix="/chat"
+)
+
 
 if __name__ == "__main__":
     app.run(
